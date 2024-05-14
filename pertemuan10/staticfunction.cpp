7040840aee3e9c37699a29b0b81f51fc20d89a4a
MannantaB <mannanta18@gmail.com>
2024-05-14 15:13:29 +0700

    memperbaiki codingan static function

--- pertemuan10/staticfunction.cpp
+++ pertemuan10/staticfunction.cpp
@@ -42,7 +42,7 @@ int main()
 {
 	mahasiswa mhs1("Sri Dadi");
 	mahasiswa mhs2("Budi Jatmiko");
-	mahasiswa::setNim(9);   //mengakakses nim melalui static member function "setNim"
+	mahasiswa::setNim(228);   //mengakakses nim melalui static member function "setNim"
 	mahasiswa mhs3("Andi Janu");
 	mahasiswa mhs4("Joko Wahono");
 
